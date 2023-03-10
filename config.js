bdd3d188cde9ace425f568817255ee22d45f85cb
Dieckman, Eric <eric.dieckman@wisc.edu>
2023-03-10 14:17:50 -0600

    initial commit with generic-http template, using part of panasonic-projector

---
+++ config.js
@@ -0,0 +1,31 @@
+
+export const configFields = [
+	{
+		type: 'textinput',
+		id: 'ip',
+		label: 'IP Address',
+		width: 4,
+		default: '',
+	},
+	{
+		type: 'textinput',
+		id: 'port',
+		label: 'Port',
+		width: 4,
+		default: '80',
+	},
+	{
+		type: 'textinput',
+		id: 'username',
+		label: 'Username',
+		width: 4,
+		default: 'admin',
+	},
+	{
+		type: 'textinput',
+		id: 'password',
+		label: 'Password',
+		width: 4,
+		default: '',
+	}
+]
